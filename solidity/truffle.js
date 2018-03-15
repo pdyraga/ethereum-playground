b9193448cfa4c9b6a5ee7a8ed9e9fbb5df4933dc
Piotr Dyraga <piotr@ontheserverside.com>
2018-03-15 15:10:12 +0100

    Configured development and rinkeby networks for Truffle

--- solidity/truffle.js
+++ solidity/truffle.js
@@ -1,4 +1,16 @@
 module.exports = {
-  // See <http://truffleframework.com/docs/advanced/configuration>
-  // to customize your Truffle configuration!
+  networks: {
+    development: {
+      host: "localhost",
+      port: 9545,
+      network_id: "*"
+    },
+    rinkeby: {
+      host: "localhost", 
+      port: 8545,
+      from: "0xa46603651a444307c4a36dccce939ed922a51173",
+      network_id: 4,
+      gas: 470000
+    }
+  }
 };
