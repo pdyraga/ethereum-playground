d3a9e08ce1d3618fe7af3bbdd8bb9246c1305cda
Piotr Dyraga <piotr@ontheserverside.com>
2018-03-16 16:36:19 +0100

    Go source files moved to the main project directory (one level up)

--- generate-bindings.sh
+++ generate-bindings.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 rm -rf contracts
 mkdir contracts contracts/abi
-solc --overwrite -o contracts/abi --abi ../solidity/contracts/SimpleGreeter.sol
+solc --overwrite -o contracts/abi --abi solidity/contracts/SimpleGreeter.sol
 abigen --abi contracts/abi/SimpleGreeter.abi --pkg contracts --type SimpleGreeter --out contracts/SimpleGreeter.go
